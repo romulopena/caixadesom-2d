c5cc0262dd76487b8f9b66f489b7399984486caf
PROF ROMULO <rjlpena@gmail.com>
2023-10-17 11:04:23 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+//Criando/declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
+//chamando/invocando a função tocaSomPom pelo clique do botão pom
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
+//criando referência constante listaDeTeclas buscando 
+//todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//invocando a função tocaSomPom a partir do item "0", 1º botão.
 listaDeTeclas[0].onclick = tocaSomPom;
