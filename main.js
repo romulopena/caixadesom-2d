ec3f8dce5090c8617ac313ac3b40d6326243612c
PROF ROMULO <rjlpena@gmail.com>
2023-11-14 11:13:22 -0300

    onkeydown

--- main.js
+++ main.js
@@ -14,4 +14,7 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
     tecla.onclick = function(){ //cria função anônima para chamar tocaSom
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
 }
