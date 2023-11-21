3e2dc42723911c6feeef914c6368bce3daf4af3b
PROF ROMULO <rjlpena@gmail.com>
2023-11-21 11:24:19 -0300

    if else elemento operadores comparação

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 //Criando/declarando a função tocaSom com parâmetro idElementoAudio
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    //if else elemento operadores comparação
+        const elemento = document.querySelector(idElementoAudio);
+        if(elemento && elemento.localName === 'audio'){
+           elemento.play(); 
+        }else{
+        console.log("Elemento não encontrado");
+    }
 }
+
 //criando referência constante listaDeTeclas buscando 
 //todos os itens pela classe .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,7 +22,7 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
         tocaSom(idAudio);
     }
     tecla.onkeydown = function(evento){
-        if(evento.code != 'Tab'){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
             tecla.classList.add('ativa');
         }  
     }
